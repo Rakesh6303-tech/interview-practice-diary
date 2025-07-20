9d485e52dd52d5a20df4942c35da28f52ff7d3bb
Rakesh <dr8452799@gmail.com>
2025-07-21 00:07:28 +0530

    Update Famil.py

--- Intro/Famil.py
+++ Intro/Famil.py
@@ -1,6 +1,6 @@
 I come From a Humble & Hard Working Family.
 We are 5 Members in total. My Parents and 3 Brothers including me.
 Im the Eldest among the Siblings. My Father & Mother Both are Farmers.
-And our Family Primarily Depends on Agriculture for our LiveliHood.
+And our Family Primarily Depends on Agriculture for our Livelihood.
 My Younger Brothers Currently Pursuing their Education.
 My Family has Always Supported and Encouraged in My Academic and Carrer Goals.
