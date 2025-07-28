f17ea9996bb92be9dfb840a0e478e49b5890ea6f
Rakesh <dr8452799@gmail.com>
2025-07-28 11:32:16 +0530

    Update THMRound.py

--- THMRound.py
+++ THMRound.py
@@ -8,20 +8,26 @@ How To Answer Sceniro Based Questions?
 How to End With Techinal Interview?
   * Thank You for ur Time and the Opportunity to speak with You. It was a Great Experience Discussing Techincal Concepts.
   * Im Genuienly Exicted about the Opportunity to work with your Team & Contribute My Skills.
+---------------------------------------------------------------------------------------------------------------------------
 
 How To End With HR Round?
   * Thank You So Much for Your Time & For Sharing More About Company Role.
   * I Really Enjoyed our Conversation & I'm Genuinely excited about the opportunity.
   * I look Forward to hearing From You !
+-----------------------------------------------------------------------------------------------------------------------------
 
 If You need Some Time To Think About Answer In Interviews
   * Can I Take A Moment To Think About This?
+-----------------------------------------------------------------------------------------------------------------------------
 
 If You Dont Know the Answer In TR?
   * Im Not Sure About The Exact Answer, But I'm Exicted about the opportunity to explore it further and learn.
+------------------------------------------------------------------------------------------------------------------------------
 
 If You Don't Know Answer in HR?
-  * I'm Not Sure About the exact Answer right now, but it honestly sounds really interesting. I'd Love the chance to explore it and learn more.
+  * I'm Not Sure About the exact Answer right now, but it honestly sounds really interesting. 
+    I'd Love the chance to explore it and learn more.
+------------------------------------------------------------------------------------------------------------------------------
 
 
 
