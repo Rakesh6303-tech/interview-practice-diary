229da0275f4793d94eb63284883db9ecf21b222f
Rakesh <dr8452799@gmail.com>
2025-07-28 11:36:33 +0530

    Update HrMround.py

--- HrMround.py
+++ HrMround.py
@@ -5,20 +5,22 @@
 తెలివైన, సమర్థవంతమైన పరిష్కారాలు మరియు స్పష్టమైన దిశను అందించగల నాయకుడిగా చూస్తే గర్వంగా ఉంటుంది.")
                                            (OR)
    I want To be A strong & Realiable Developers and lead project as an Manager, and be someone the team can depend on for smarter solutions
-                                          
-
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 2) What is Your Strengths?
   * I'm a Self Motivated Person, Quick Learner, and adaptable to new Environments.
   //* Im very much confident &  Dedicated to My work and I have more Patience.
+-------------------------------------------------------------------------------------------------------------------------------------------------------
   
 3) what is Your Weakness?
   * Sometimes I focus too much on perfection, even when the work is already good enough. 
     I'm learning to balance quality with efficiency by managing priorities and deadlines better."
    ( "కొన్నిసార్లు పనిని చాలా పర్ఫెక్ట్ చేయాలని ఎక్కువ సమయం కేటాయిస్తాను. ఇప్పుడు నేను ప్రాధాన్యతలు, గడువులపై దృష్టి పెట్టి సమర్థవంతంగా పని చేయడం నేర్చుకుంటున్నాను.)
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 4) Why Should We Hire You?
   * As Im a Fresher with Analytical Skills & Theoretical Knowledge and Good Communication Skills. 
       IF you Hire Me I can Show My Skills Towards My Work. I think this is the best Platform to start My Carrrer
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 5) Why Do You Want To Work For Our Company?
    * As a fresher, I’m looking for a company that supports learning and development. 
@@ -26,6 +28,7 @@
            I believe this would be a great place to start and build my career.”
                        (OR)
    * I've Always Admired Your Company Commitment to Innovation & Quality. I Belive that your values align with my passion for continuous Learning & Improvement.
+------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 6) How do you handle multiple tasks or deadlines at the same time?
    * I would break down both projects, prioritize tasks based on urgency and impact, and plan my time carefully. 
@@ -35,41 +38,51 @@
 
 ---> “If both projects are high priority, I’d see if I can delegate or collaborate with team members to share the load. 
           I’d communicate with stakeholders, manage expectations, and stay focused on delivering both on time.”
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 7) If you are a Manager How Manage?
    * I would Focus on Clear Communication , Team Work and respect I would make Sure EveryOne Understand their Tasks and Goals. 
       I'd listen to my Team's ideas and supported Them When They need help as Belive Happy & Motivated Team Always Work Better.
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 8) Short Term Goal?
     My STG is to get a job where i can learn & Grow as A Developer
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 9) Long Term Goal?
     My LTG is to become an expert in my field & Take on LeaderShip Roles.
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 10) Describe a situation where you showed Leadership?
     when my team was confused during a task, I stepped up, divided the work clearly, & motivated everyone, 
     we finished the task smoothly as a result.
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 11)  Tell Me About a Time You Had to Meet a Tight Deadline?
     I planned my tasks, avoided distractions, and focused on completing high-priority work first with proper,  
     Time Management I was able the Deadline Successully.
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 12)  Do You Have Any Questions?
      Yes I'd Love to know more about the kind of projects the team is working? 
      what a typical day looks like for a Developer in this Role?
      How would you describe the company culture?
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 13) Are You Open To Relocation (or) Working in Shifts?
      Yes, Im open to relocation and working in shifts if the role Demands it.
      I Believe flexibility is Important early in a carrer to learn & Grow".
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 14) How do you resolve conflicts among team members?
     "I handle team conflicts by staying calm, listening to all perspectives, and encouraging open, respectful communication.
      I focus on finding a fair solution that supports team goals and maintains collaboration." 
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 15) How do you motivate your team especially during high pressure situations
     "During high-pressure situations, I keep the team focused by breaking tasks into manageable parts, setting clear priorities, and maintaining a positive mindset. 
     I encourage and support my teammates, celebrate small wins, and remind them of the bigger goal to keep motivation high.
+-------------------------------------------------------------------------------------------------------------------------------------------------------
 
 16)  If you were a TL how do you support and motivate ?
      "As a team leader, I focus on three core areas to support and motivate my team: communication, ownership, and growth."
