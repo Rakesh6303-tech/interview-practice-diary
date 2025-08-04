2f5932e4c7bbeb19c9e03a4c4014251a18e99e01
Rakesh <dr8452799@gmail.com>
2025-08-04 20:06:40 +0200

    Create BackendEms.py

---
+++ Projects/BackendEms.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+**Project Title: Employee Management System**
+
+My project is titled **Employee Management System**, developed using **Java**, **Spring Boot**, **MySQL**, and **REST APIs**.
+
+It’s a **backend-focused web application** that allows organizations to manage employee data efficiently. The core functionalities include **adding new employees, viewing records, updating details, and deleting employees**—all handled through RESTful APIs.
+
+I used **Spring Boot** with **Spring Data JPA** to interact with the MySQL database, enabling smooth data operations and API communication. The APIs were thoroughly tested using **Postman** to ensure proper functionality and error handling.
+
+The application follows a **layered architecture** (Controller, Service, Repository), promoting modular code and scalability. All employee data is securely stored and managed in a **MySQL** database.
+
+Through this project, I gained practical experience in **API development**, **CRUD operations**, **database connectivity**, and **real-world backend system design** using Spring Boot.
+
