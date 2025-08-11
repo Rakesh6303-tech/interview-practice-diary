0bd4d4d14f464b874cf9c5f0be7dc6bbf824fa18
Rakesh <dr8452799@gmail.com>
2025-08-11 23:42:28 +0530

    Update Online Food System.py

--- Projects/Online Food System.py
+++ Projects/Online Food System.py
@@ -1,6 +1,7 @@
 
-About : My Project Name Is OFS. It is a Web Application . I Build By using Java, Jdbc, Servlets, JSsP & MYSQL. 
-        It Handles Features Like Food Ordering, USer Login, Restart lIstings & Cart.
+About : My Project Name Is OFS. It is a Web Application . 
+         I Build By using Java, Jdbc, Servlets, JSsP & MYSQL. 
+        It Handles Food Ordering with Features Like Restaurent Listings, USer Login,  & Cart.
 -----------------------------------------------------------------------------------------------------------
           
 Technologies : 
