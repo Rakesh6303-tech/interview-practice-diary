459a991881eb826a985f515562d06ae84e8887ed
Rakesh <dr8452799@gmail.com>
2025-10-11 23:50:51 +0530

    Update Self.py

--- Intro/Self.py
+++ Intro/Self.py
@@ -3,7 +3,7 @@ My Name is Rakesh. Im Belongs to Mantralyam Where Im Completed My Schooling & In
 I hold's a Bachelor Degree in Computer Science Engineering.
 My Technical Skills Include in Java, Spring Boot, MySQL, as well as Web Technologies like HTML, CSS and Bootstrap.
 Im Completed JFW Course From TapAcademy. Where I Also Gained Practical Knowledge in OOPS , Exception Handling , Multi Threading , & Collections.
-As a Part of My Learning Journey I Developed EMS with Spring Boot, React.
+As a Part of My Learning Journey I Developed EMS with Spring Boot, RestAPI.
 To Strength My Skills Further I earned a Java Certification From Hacker Rank.
 Apart From Academics I Secured an AIR 28k among 5Lakh Participations in Naukri Young Campus Competion and Won FootBAll ChampionShip Which Reflecting My Team Work and Discipline.
 I'm Eager to Learn and contribute meaningfully as a Software Developer in your Estemmed Organization.
